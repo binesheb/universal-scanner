4776f787b874960706f9bd0bffe01e548182bc65
Binesh Ellupurayil Balachandran <31845423+binesheb@users.noreply.github.com>
2026-08-23 21:49:16 +0530

    Fix missing Activity Result dependency and bump build version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,13 +11,14 @@ android {
         applicationId = "com.binesheb.universalscanner"
         minSdk = 23
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.camera:camera-core:1.4.1")
